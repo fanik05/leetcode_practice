46d7dc29661a37ee7e7c78666b6be23200315d36
fanik05 <fatinanik129@gmail.com>
2026-02-22 16:05:29 +0600

    Solved hashset problem with linked list

--- design_hashset.py
+++ design_hashset.py
@@ -26,6 +26,39 @@ class MyHashsetWithBooleanArray:
     def contains(self, key: int) -> bool:
         return self.data[key]
 
+class ListNode:
+    def __init__(self, key: int):
+        self.key = key
+        self.next = None
+    
+class MyHashSetWithLinkedList:
+    def __init__(self):
+        self.set = [ListNode(0) for _ in range(10**4)]
+
+    def add(self, key: int) -> None:
+        cur = self.set[key % len(self.set)]
+        while cur.next:
+            if cur.next.key == key:
+                return
+            cur = cur.next
+        cur.next = ListNode(key)
+
+    def remove(self, key: int) -> bool:
+        cur = self.set[key % len(self.set)]
+        while cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
+                return
+            cur = cur.next
+
+    def contains(self, key: int) -> bool:
+        cur = self.set[key % len(self.set)]
+        while cur.next:
+            if cur.next.key == key:
+                return True
+            cur = cur.next
+        return False
+
 # test cases
 hashSet = MyHashSet()
 hashSet.add(1)
@@ -45,4 +78,12 @@ hashSet2.add(2)
 print(hashSet2.contains(2))  # Output: True
 hashSet2.remove(2)
 print(hashSet2.contains(2))  # Output: False
-
+hashSet3 = MyHashSetWithLinkedList()
+hashSet3.add(1)
+hashSet3.add(2)
+print(hashSet3.contains(1))  # Output: True
+print(hashSet3.contains(3))  # Output: False
+hashSet3.add(2)
+print(hashSet3.contains(2))  # Output: True
+hashSet3.remove(2)
+print(hashSet3.contains(2)) # Output: False
